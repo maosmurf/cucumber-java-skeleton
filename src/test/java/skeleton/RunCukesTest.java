105e1589b797c35203b1aaf813c7184549c3df5a
xza <xza@localhost>
2014-06-25 15:24:11 +0200

    with scenario outline

--- src/test/java/skeleton/RunCukesTest.java
+++ src/test/java/skeleton/RunCukesTest.java
@@ -5,6 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(monochrome = true)
+@CucumberOptions(monochrome = true,
+format="json:target/cucumber.json")
 public class RunCukesTest {
 }
--- src/test/java/skeleton/Stepdefs.java
+++ src/test/java/skeleton/Stepdefs.java
@@ -1,11 +1,19 @@
 package skeleton;
 
+import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.When;
 
 public class Stepdefs {
     @Given("^I have (\\d+) cukes in my belly$")
     public void I_have_cukes_in_my_belly(int cukes) throws Throwable {
         Belly belly = new Belly();
         belly.eat(cukes);
+    }
+
+    @When("^I eat (.*)$")
+    public void iEatFood(String food) throws Throwable
+    {
+        System.out.println("I eat " + food);
     }
 }
